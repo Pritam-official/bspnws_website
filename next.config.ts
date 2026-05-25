a7168aaa1521028fc747e57f9a096d208ff94875
pritam2002ghosh <pritam.ghosh@edgewrapper.com>
2026-05-25 18:01:11 +0530

    next.config push

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  experimental: {
+    // @ts-ignore
+    allowedDevOrigins: ["surrogate-onlooker-graveyard.ngrok-free.dev"],
+  },
+  reactCompiler: false,
 };
 
 export default nextConfig;
